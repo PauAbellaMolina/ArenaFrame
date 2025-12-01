a9b8f41da077a15adf038fd01eb13e5ffe3b0b7d
Pau Abella <pauabellamolina1@gmail.com>
2025-12-01 01:18:00 +0100

    better authorize button

--- app/components/buttons/AuthorizeButton.tsx
+++ app/components/buttons/AuthorizeButton.tsx
@@ -9,9 +9,9 @@ export default function AuthorizeButton({
     <div className="flex min-h-screen items-center justify-center">
       <button
         onClick={handleAuthorize}
-        className="rounded-md border border-zinc-300 bg-white px-4 py-2 text-sm shadow-sm hover:bg-zinc-100 dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-800"
+        className="rounded-xl border border-zinc-300 bg-zinc-100 px-4 py-2 text-md shadow-sm hover:bg-zinc-200 dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-100 dark:hover:bg-zinc-800 cursor-pointer transition-all"
       >
-        Authorize
+        Login with Are.na
       </button>
     </div>
   );
